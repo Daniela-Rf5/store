cb0a1e6524fb0509edc254bc85e5bf17b2c96b74
Daniela-Rf5 <danielarodriguezlainez@gmail.com>
2021-05-18 14:49:55 +0200

    añadiendo metodos

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -1,15 +1,28 @@
 package org.factoriaf5.store.controllers;
 
+import org.factoriaf5.store.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 
 public class ProductController {
-   @GetMapping("/health")
 
-   public String check(){
-        String greeting = "Hello! I'm server.";
-                return greeting;
+  private List<Product> products;
+  public  ProductController(){products = new ArrayList<>(); }
+
+
+   @GetMapping("/products")
+   public List<Product> allProducts(){
+        return products;
     };
+
+   @PostMapping("/products")
+    public void addProduct(Product p){
+       products.add(p);
+   }
 };
